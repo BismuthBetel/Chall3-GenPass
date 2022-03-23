8b5cf1bf39ca0730fa64584d380e1c6cb2bde813
BismuthBetel <97366538+BismuthBetel@users.noreply.github.com>
2022-03-23 14:12:14 -0500

    added prompt when button is clicked

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,42 @@
 // Assignment code here
+var characters = Math.random().toString(36).slice(-8);
 
+// window.prompt("Generate a random password!");
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function forPrompt() {
+  let generateBtn = prompt("Please select the criteria for your password.");
+  alert(generateBtn);
+}
+
+let randomNumbers = function() {
+
+}
+
+let randomLetters = function()
+{ }
+  
+  // let generatePassword = function() {
+//   if ( )
+// }
+
 // Write password to the #password input
 function writePassword() {
+  let generatePassword = Math.random().toString(36).slice(-8);
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  // characters = password;
+  
   passwordText.value = password;
+
+  // console.log("The password is" + writePassword + ".");
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+writePassword();
