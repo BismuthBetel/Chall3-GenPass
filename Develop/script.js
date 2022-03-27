321694d8940bb3a2db136774f9f08776d8b5d30b
BismuthBetel <97366538+BismuthBetel@users.noreply.github.com>
2022-03-27 00:08:15 -0500

    added prompts for character options, generatePassword function, and added Readme

--- Develop/script.js
+++ Develop/script.js
@@ -1,36 +1,119 @@
 // Assignment code here
-var characters = Math.random().toString(36).slice(-8);
 
-// window.prompt("Generate a random password!");
+//Character Arrays
+
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCase = ["a", "b", "c", "d", "e,", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
+let numberChoices = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let specChar = ["!", "@", "#", "$", "%", "^", "&", "*"];
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-function forPrompt() {
-  let generateBtn = prompt("Please select the criteria for your password.");
-  alert(generateBtn);
+//function for prompts
+var promptCriteria = function () {
+
+  var characterLength = Number(window.prompt("How many characters would you like in your password? Choose between at least 8 characters and less than 128 characters"));
+
+  if (characterLength < 8 || characterLength > 128) {
+    alert("Password must be between 8 and 128 characters.")
+
+    return null;
+  }
+
+  var hasUpperCharacters = confirm("Click ok if you would like uppercase characters.")
+
+  var hasLower = confirm("Click ok for lower case characters.")
+
+  var hasNumbers = confirm("Click ok for numbers.")
+  var hasSpecCharacters = confirm("Click ok for special characters.")
+
+  if (hasUpperCharacters === false && hasLower === false && hasNumbers === false && hasSpecCharacters === false) {
+    alert("Must choose at least one option.");
+    return null;
+  }
+
+  var passwordCriteriaObj = {
+    length: characterLength,
+    upperCaseOpt: hasUpperCharacters,
+    lowerCaseOpt: hasLower,
+    numberOpt: hasNumbers,
+    specialCharacOpt: hasSpecCharacters
+  }
+  return passwordCriteriaObj
+
 }
 
-let randomNumbers = function() {
+var generatePassword = function () {
+
+  var passwordOptions = promptCriteria();
+  console.log(passwordOptions);
+
+
+  var possibleCharactersArray = [];
+
+
+  if (passwordOptions.upperCaseOpt) {
+    possibleCharactersArray = possibleCharactersArray.concat(upperCase)
+
+    possibleCharactersArray.push(upperCase[Math.floor(Math.random() * upperCase.length)])
+    //guaratneeCharacters.push(upperCase[Math.floor(Math.random() * upperCase.length)])
+    //console.log(possibleCharactersArray)
+  }
+
+  if (passwordOptions.lowerCaseOpt) {
+    possibleCharactersArray = possibleCharactersArray.concat(lowerCase)
+
+    possibleCharactersArray.push(lowerCase[Math.floor(Math.random() * lowerCase.length)])
+    //console.log(possibleCharactersArray)
+    //guaratneeCharacters.push(lowerCase[Math.floor(Math.random() * lowerCase.length)])
+    //console.log(guaratneeCharacters)
+
+  }
+
+  if (passwordOptions.numberOpt) {
+    possibleCharactersArray = possibleCharactersArray.concat(numberChoices)
+
+    possibleCharactersArray.push(numberChoices[Math.floor(Math.random() * numberChoices.length)])
+    //console.log(possibleCharactersArray)
+    //guaratneeCharacters.push(numberChoices[Math.floor(Math.random() * numberChoices.length)])
+    //console.log(guaratneeCharacters)
+  }
+
+  if (passwordOptions.specialCharacOpt) {
+    possibleCharactersArray = possibleCharactersArray.concat(specChar)
+
+    possibleCharactersArray.push(specChar[Math.floor(Math.random() * specChar.length)])
+
+    //console.log(possibleCharactersArray)
+    //guaratneeCharacters.push(specChar[Math.floor(Math.random() * specChar.length)])
+    // console.log(guaratneeCharacters)
+  }
+
+  for (var i = 0; i < possibleCharactersArray.length; i++) {
+    //promptCriteria(characterLength);
+
+
+    possibleCharactersArray.push(possibleCharactersArray[Math.floor(Math.random() * possibleCharactersArray.length)])
+    console.log(possibleCharactersArray);
+  }
 
 }
 
-let randomLetters = function()
-{ }
-  
-  // let generatePassword = function() {
-//   if ( )
-// }
 
 // Write password to the #password input
 function writePassword() {
-  let generatePassword = Math.random().toString(36).slice(-8);
+
   var password = generatePassword();
+  console.log(password)
   var passwordText = document.querySelector("#password");
 
-  // characters = password;
-  
+
+
   passwordText.value = password;
+
+  alert(password)
 
   // console.log("The password is" + writePassword + ".");
 
@@ -39,4 +122,3 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-writePassword();
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Password Generator Starter Code
+
+## Purpose
+
+This application's purpose is to generate a random password with the length between 8 and 28 characters, and with the option to include lower case, upper case, numbers and/ or special characters.
+
+## Built with:
+
+*HTML
+*CSS
+\*JavaScript
+
+## Website link
+
+https://bismuthbetel.github.io/Chall3-GenPass/
+
+## Image of Website ![Password_Application_Web_image](https://user-images.githubusercontent.com/97366538/160267444-3f708841-42eb-4a22-bff2-7a8df32f5c65.png)
+
+### Contributor
+
+Starter code made by Xander Rapstine. JavaScript added by Rebecca Holmberg.
+
+![HTML-239120 Badge](https://user-images.githubusercontent.com/97366538/157144164-92469c7b-00cd-43df-87fb-4b47f0edf1b0.svg)
+![CSS-239120 Badge](https://user-images.githubusercontent.com/97366538/157144352-16f156c0-3b24-4e17-9067-c5f87e34c559.svg)
+![JavaScript-F7DF1E Badge](https://user-images.githubusercontent.com/97366538/160267535-04b7dbe5-b68e-4d83-8adc-89fd73623a09.svg)
